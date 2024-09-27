ed173979c929f6c1df56a10315adcfff45db8d27
Bernhard <beloureiro@gmail.com>
2024-09-27 17:42:22 +0100

    Tentando voltar para encontrar a confusao que o o1-mini me fez

--- sections/__init__.py
+++ sections/__init__.py
@@ -1,2 +1,12 @@
+from .ai_crew import ai_crew_component
 from .overview import show_overview
-from .ai_agents import show_ai_agents
+from .feedback_analysis import patient_feedback_analyzer
+from .use_case import ai_clinical_advisory_crew_tab
+
+__all__ = [
+    'ai_crew_component',
+    'show_overview',
+    'patient_feedback_analyzer',
+    'ai_clinical_advisory_crew_tab'
+]
+
