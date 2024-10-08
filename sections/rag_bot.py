01a7ee1d35eb35b24a1e80ad518f626d219fc617
Bernhard <beloureiro@gmail.com>
2024-10-08 21:09:19 +0100

    added ragbot img

--- sections/rag_bot.py
+++ sections/rag_bot.py
@@ -2,5 +2,8 @@ import streamlit as st  # type: ignore
 
 def rag_bot_component():
     st.title("AI-Skills Advisor")
-    st.write("This is the AI-Skills Advisor component.")
+    
+    # Exibe a imagem RagBot.png
+    st.image("assets/agents/RagBot.png", caption="RAGBot Healthcare Professional Coach", use_column_width=True)
+    
     # Adicione mais lógica aqui conforme necessário
